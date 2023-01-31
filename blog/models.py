ab1e45fdbcd8c930bfe2ac74500d41165afc6055
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2023-01-31 21:59:10 +0100

    Add model blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField, DateField
+from django.db.models.fields.files import ImageField
+import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    title = CharField(max_length=120)
+    description = TextField(default='')
+    image = ImageField(upload_to="blog/images/")
+    date = DateField(datetime.date.today)
